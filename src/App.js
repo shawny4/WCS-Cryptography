db48f2b54f7213f8f4d92670aef73dc667a21237
Grzrzegorzrz <100858542+Grzrzegorzrz@users.noreply.github.com>
2024-02-26 19:21:16 -0500

    Web - Updated Code

--- src/App.js
+++ src/App.js
@@ -1,60 +1,38 @@
 import './App.css';
 import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom';
-
-import Shawn from './Pages/Shawn-BlockCipher';
-import Vanson from './Pages/Vanson';
-
+import BlockCipher from './Pages/BlockCipher';
 import BlockChainAndRNG from './Pages/BlockChainAndRNG';
-
-import Alex from './Pages/Alex'
-
 import Duck from './Duck';
+//import duck from './images/quack.png';
 
 function App() {
-  
   return (
     <BrowserRouter>
     
       <div className="App"> 
-      <header className="App-header">
+        <header>
           <nav>
             <NavLink to ="idkLol"> uhhhhh</NavLink>
-
-          
-
-            <NavLink to="Shawn-blockCipher">Shawn Yuen</NavLink>
-            <NavLink to="Vanson-cipher">Vanson</NavLink>
-
-           
-            <NavLink to="Web-block"> Web</NavLink>
-
-            <NavLink to="Alex-cipher">Alex</NavLink>
-
+            <NavLink to="blockCipher">Block Cipher</NavLink>
+            <NavLink to="otherBlock"> Other Block</NavLink>
             <NavLink to="/">Home</NavLink>
-
             
-            
+            <h1>THIS IS A NAV BAR</h1>
           </nav>
         </header>
         
         <main>
         
-        <div className="content">
-        <h1>WCS Cryptography Implementation</h1>
+        <div classname="content">
+          <h1>Hello World</h1>
         
           
           
           <Routes>
             <Route path="/"/>
             <Route path="/idkLol" element={<Duck/>}/>
-          
-            <Route path="Web-block" element={<BlockChainAndRNG/>} />
-
-
-            <Route path="Shawn-blockCipher" element={<Shawn/>} />
-            <Route path="Vanson-cipher" element={<Vanson/>}/>
-
-            <Route path="Alex-cipher" element= {<Alex/>}/>
+            <Route path="blockCipher" element={<BlockCipher/>} />
+            <Route path="otherBlock" element={<BlockChainAndRNG/>} />
 
           </Routes> 
           
