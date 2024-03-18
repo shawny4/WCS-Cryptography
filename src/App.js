df0c97386f923fc75845394632a95b28e8fc45e9
alexdng10 <hungqdang0510@gmail.com>
2024-03-18 14:29:53 -0400

    add spaces

--- src/App.js
+++ src/App.js
@@ -46,6 +46,9 @@ function App() {
               <img src={logo} className={`App-logo ${startAnimation ? 'fade-in' : ''}`} alt="Logo" />
               <nav>
                 <NavLink to="/idkLol">uhhhhh</NavLink>
+                <NavLink to="/idkLol">uhhhhh</NavLink>
+                
+                <NavLink to="/Vanya">Vanya</NavLink>
                 <NavLink to="/Vanson">Vanson</NavLink>
                 <NavLink to="/Shawn">Shawn</NavLink>
                 <NavLink to="/Web">Web</NavLink>
