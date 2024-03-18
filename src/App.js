7fbc9b7eef70d813dec0218cb57a760b8a67aea1
alexdng10 <hungqdang0510@gmail.com>
2024-03-18 14:22:52 -0400

    changed tiny stuff to make it smooth

--- src/App.js
+++ src/App.js
@@ -45,11 +45,13 @@ function App() {
               <img src={logo} className={`App-logo ${startAnimation ? 'fade-in' : ''}`} alt="Logo" />
               <nav>
                 <NavLink to="/idkLol">uhhhhh</NavLink>
+                
+                
+                <NavLink to="/Vanya">Vanya</NavLink>
                 <NavLink to="/Vanson">Vanson</NavLink>
                 <NavLink to="/Shawn">Shawn</NavLink>
                 <NavLink to="/Web">Web</NavLink>
                 <NavLink to="/Alex">Alex</NavLink>
-                <NavLink to="/Vanya">Vanya</NavLink>
                 <NavLink to="/">Home</NavLink>
               </nav>
             </header>
